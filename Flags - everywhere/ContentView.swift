24c675ab73820e4506ebcce18b7829c9ac5b6c46
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-17 14:09:26 -0400

    Add Accessibility - VoiceOver on flags

--- Flags - everywhere/ContentView.swift
+++ Flags - everywhere/ContentView.swift
@@ -22,6 +22,20 @@ struct ContentView: View {
     @State private var holder: String = ""
     @State private var index: Int? = nil
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var gradient = MeshGradient(width: 3, height: 3, points: [
         [0, 0], [0.5, 0], [1, 0],
         [0, 0.5], [0.5, 0.5], [1, 0.5],
@@ -76,8 +90,10 @@ struct ContentView: View {
                                 FlagImage(index: number, countries: countries, currectIndex: currectIndex, tapped: isTapped, tappedIndex: index)
                                 
                             }
+                            .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                             
                         }
+                        
                         .animation(.easeInOut(duration: 1))
                         
                         
